b0441c770ca71aa6681cffe59453b9785cb28d56
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2025-04-05 01:08:10 +0200

    removing one boucle

--- easy/1672RichestCustomer/1672Solution.py
+++ easy/1672RichestCustomer/1672Solution.py
@@ -3,9 +3,7 @@ class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         res = 0
         for customer in range(0, len(accounts)): 
-            sum = 0
-            for wealth in range(0,len(accounts[customer])):
-                sum += accounts[customer][wealth]
-            if(sum > res):
-                res = sum
+            sumCompare = sum(accounts[customer])
+            if(sumCompare > res):
+                res = sumCompare
         return res
